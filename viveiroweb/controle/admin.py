492ef498b7bc861d92043ac23b0c288c1acbed71
Matheus-oliKelvin <matheuskelvin309@gmail.com>
2025-06-17 21:41:51 -0300

    Registro do modelo Planta

--- viveiroweb/controle/admin.py
+++ viveiroweb/controle/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Planta
 
 # Register your models here.
+@admin.register(Planta)
+class PlantaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'nomeclatura', 'familia', 'nutricao', 'regiao', 'nativa', 'manejo')
+    search_fields = ('nome', 'nomeclatura', 'familia','regiao')
+    list_filter = ('nativa',)
+    ordering = ('nome',)
