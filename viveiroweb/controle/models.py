15ad86ad023aa1deff6cd45cf337b1098be6ae5f
Matheus-oliKelvin <matheuskelvin309@gmail.com>
2025-06-17 21:14:57 -0300

    Criação do modelo Planta

--- viveiroweb/controle/models.py
+++ viveiroweb/controle/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Planta(models.Model):
+    id = models.AutoField(primary_key=True) 
+    nome= models.CharField(max_length=250, unique=True) #unico
+    nomeclatura = models.CharField(max_length=250, unique=True) #unico
+    familia = models.CharField(max_length=250)
+    nutricao = models.CharField(max_length=250)
+    regiao = models.CharField(max_length=250)
+    nativa = models.BooleanField(default=False) #não sabemos qual o critério para definir se é nativa ou não
+    manejo = models.CharField(max_length=250) #depois pode ser um link de um video
+    # pragas e doenças podem entrar depois
+
+    def __str__(self):
+        return self.nome
+    
