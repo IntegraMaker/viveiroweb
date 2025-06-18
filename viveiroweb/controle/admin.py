8970bd9b482820153134cce77434060f17379603
ronnedoobee <macenaronne@gmail.com>
2025-06-18 17:34:19 -0300

    Criação do modelo Reserva

--- viveiroweb/controle/admin.py
+++ viveiroweb/controle/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Planta
+from .models import Planta, Reserva
 
 # Register your models here.
 @admin.register(Planta)
@@ -8,3 +8,10 @@ class PlantaAdmin(admin.ModelAdmin):
     search_fields = ('nome', 'nomeclatura', 'familia','regiao')
     list_filter = ('nativa',)
     ordering = ('nome',)
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'solicitante', 'data', 'motivo', 'obs', 'aceito', 'email')
+    search_fields = ('solicitante', 'data', 'motivo')
+    list_filter = ('data',)
+    ordering = ('data',)
