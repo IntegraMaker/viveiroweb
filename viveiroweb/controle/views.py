c3434d168147f4fbaa59ac67386c2c4fbcfefdef
ronnedoobee <macenaronne@gmail.com>
2025-07-17 16:35:16 -0300

    Mudanças para página principal

--- viveiroweb/controle/views.py
+++ viveiroweb/controle/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Planta
 
 # Create your views here.
+
+def home(request):
+    plantas_aleatorias = Planta.objects.order_by('?')[:3]
+    return render(request, 'home.html', {'plantas': plantas_aleatorias})
