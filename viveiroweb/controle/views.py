54a4412612cd3e7fe3d9fcfbdd8b5734e9e70dc7
Matheus-oliKelvin <matheuskelvin309@gmail.com>
2025-08-09 21:16:02 -0300

    primeira versão da pagina de formulario_visita

--- viveiroweb/controle/views.py
+++ viveiroweb/controle/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render #, get_list_or_404 
+#from django.contrib.auth.decorators import login_required // isso vai dizer que a rota só pode ser acessada por usuários logados
 
 # Create your views here.
+
+
