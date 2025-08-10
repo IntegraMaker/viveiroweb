da33fdf9ee6063f13fcca13f8d65ddf7a789642b
ronnedoobee <macenaronne@gmail.com>
2025-08-10 07:29:02 -0300

    Página Principal e Catálogo (nao final)

--- viveiroweb/controle/views.py
+++ viveiroweb/controle/views.py
@@ -4,5 +4,10 @@ from .models import Planta
 # Create your views here.
 
 def home(request):
-    plantas_aleatorias = Planta.objects.order_by('?')[:3]
+    plantas_aleatorias = Planta.objects.order_by('?')[:12]
     return render(request, 'home.html', {'plantas': plantas_aleatorias})
+
+def catalogo(request):
+    plantas= Planta.objects.order_by('nome')
+    plantas_tamanho = len(plantas)
+    return render(request, 'catalogo.html', {'plantas': plantas, 'plantas_tamanho': plantas_tamanho})
